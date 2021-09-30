e5eb64b7c9fd4cf3b8da204ce03c69a398d08f19
tolozayurany <yalbarracint@gmail.com>
2021-09-30 18:53:46 -0500

    Se creó la función que comprueba si es archivo o directorio y la creación del objeto en caso de ser archivo

--- md-links.js
+++ md-links.js
@@ -2,59 +2,109 @@ const index = require('./index.js');
 const fsp = require('fs').promises;
 const fs = require('fs');
 const modulePath = require('path');
+const markdownLinkExtractor = require('markdown-link-extractor')
+
 
 /* Variable route que contiene el argumento pasado en la consola */
 let route = process.argv[2];
 
 /* mdLinks contiene todas las funciones del flujo */
 const mdLinks = (path) => {
-  /* 1. Comprobar si se recibe la ruta y existe */
-  routeExists(path).then(() => {
-    /* 2. Llamar función isAnDirectory() y pasarle como argumento la función isAbsolute */
-    isAnDirectory(isAbsolute(path)).then((res) => {
-      if (res === true) {
-        /* Leer el directorio */
-        readDirectory(path);
-      } else {
-        /* Leer el contenido del archivo */
-        /* Primero debe preguntar si es de extensión .md para luego leerlo */
-        readFile(path);
-      }
-    });
-  })
-    .catch(error => { console.log('error encontrado => ', error) });
+  /* 2. Llamar función isAnDirectory() y pasarle como argumento la función isAbsolute */
+  isAnDirectory(isAbsolute(path))
 }
-
-/* Comprobar si se recibe la ruta y existe */
-const routeExists = (path) => fsp.stat(path);
 
 /* Comprobar si recibe ruta absoluta o relativa */
 const isAbsolute = (path) => {
-  // si es absoluta retornar la ruta normal 
+  // Si es absoluta retornar la ruta normal 
   if (modulePath.isAbsolute(path) === true) {
     return path;
   } else {
-    // convertir la ruta a absolta
+    // Convertir la ruta a absolta
     let newPath = modulePath.resolve(__dirname, path);
     return newPath;
   }
 }
 
+/* Comprobar que la extensión del archivo sea .md */
+const isMd = (path) => modulePath.extname(path) === '.md' ? true : false;
+
 /* Comprobar si es un archivo o un directorio */
-const isAnDirectory = (path) => fsp.stat(path).then((stats) => stats.isDirectory())
+const isAnDirectory = (path) => fsp.stat(path)
+  .then((stats) => stats.isDirectory())
+  .then((res) => {
+    if (res === true) {
+      /* Leer el directorio */
+      readDirectory(path);
+      /* Si es de extensión .md se lee el archivo*/
+    } else if (isMd(path)) {
+      // ME FALTA HACER ALGO PARA CUANDO NO SEA UN ARCHIVO MD
+      /* Leer el contenido del archivo */
+      readFile(path);
+    }
+  })
   .catch(error => {
-    console.log('error dentro de isAnDirectory =>', error)
+    console.log('error dentro de isAnDirectory => ', error)
   });
+
+/*  CREACION DE UNA PROMESA
+const promise = new Promise((resolve, reject) => {
+   if (path) {
+     resolve(path)
+   } else {
+     reject(new Error('no hay ruta'))
+   }
+ })
+ 
+return promise
+LLAMAR LA PROMESA
+.then(path => console.log(path))
+.catch(error => console.error(error)); 
+ */
+
+/* Extraer los links de un archivo md */
+const extractLinks = (markdown) => markdownLinkExtractor(markdown, true);
+
+/* Por cada link extraer sus propiedades (href,text,file) */
+const links = (markdown) => {
+  const promise = new Promise((resolve, reject) => {
+    if (extractLinks(markdown)) {
+      resolve(extractLinks(markdown))
+    } else {
+      reject(new Error('no funciono'))
+    }
+  })
+  return promise;
+}
 
 /* Leer el contenido del archivo */
 const readFile = (path) => {
+  let array = [];
   fs.readFile(path, { encoding: 'utf-8' }, (error, data) => {
     if (error) {
-      console.log('ocurrió un error', error);
+      console.log(error)
     }
-    console.log('correcto', data);
+    links(data).then((res) => {  
+      res.forEach(element => {
+        let object = {
+          href: element.href,
+          text: element.text,
+          file: path,
+        }
+        array.push(object) 
+      })
+      console.log(array) // ya se crea el array de objetos con las propiedades que necesito
+      // ahora tengo un problema con el valor de retorno de esta función 
+      // revisar si puedo sacar esto a otra función para hacerla mas pura o alguna forma de retornarla 
+    })
+      .catch(error => {
+        console.log(error)
+      })
   })
+  console.log(array)
+  return array
 }
+
 
 /* Leer los archivos de un directorio */
 const readDirectory = (path) => {
@@ -70,7 +120,6 @@ const readDirectory = (path) => {
     })
 }
 
-
 /*  CREACION DE UNA PROMESA
 const promise = new Promise((resolve, reject) => {
    if (path) {
@@ -84,9 +133,7 @@ return promise
 LLAMAR LA PROMESA
 .then(path => console.log(path))
 .catch(error => console.error(error)); 
-
-// EXTRAER LA RUTA DEL ARCHIVO
-  console.log(path.extname(route)) */
+ */
 
 
 mdLinks(route);
