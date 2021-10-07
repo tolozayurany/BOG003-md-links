b96f3672b90e5b29e467cd15605c67ec52b4ec75
tolozayurany <yalbarracint@gmail.com>
2021-10-07 18:40:28 -0500

    Crear fcomando en npm y función para validar si la opcion --validate es true y agregar propiedades al objeto

--- index.js
+++ index.js
@@ -1,9 +1,25 @@
+#!/usr/bin/env node
 const mdLinks = require('./md-links.js');
 
 /* Variable route que contiene el argumento pasado en la consola */
-let route = process.argv[2];
+const route = process.argv[2];
+const options = process.argv[3];
+/* Comprobar si recibe como option --validate */
 
-mdLinks.mdLinks(route).then((res) => {
-  console.log('response in mdLinks call ', res)
+const isValidate = (options) => options === '--validate' ? true : false;
 
+mdLinks.mdLinks(route, options).then((res) => {
+ 
+  if (isValidate(options)){
+    res.map((element) => {
+      element.status = '45778'
+      element.ok = 'ok'
+      console.log(res)
+      // console.log(position);
+    });
+    
+  }
+  else {
+    console.log('response in mdLinks call ', res);
+  }
 }).catch(error => console.log('error promesa', error));
