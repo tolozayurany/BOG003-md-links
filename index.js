3c27664e0a43a4f481db8d255537052e77477243
tolozayurany <yalbarracint@gmail.com>
2021-10-12 18:06:12 -0500

    Función para hacer estadísticas al escribir el comando --stats --falta contar links únicos

--- index.js
+++ index.js
@@ -4,41 +4,67 @@ const mdLinks = require('./md-links.js');
 /* Variable route que contiene el argumento pasado en la consola */
 const route = process.argv[2];
 const options = process.argv[3];
+const option2 = process.argv[4];
 
 /* mdLinks ./readme1.md --validate */
 
-/* Comprobar si recibe como option --stats */
-const stats = (options) => options === '--stats' ? true : false;
-
-/* Ejecutar estadísticas de links cuando la opción es --stats */
-const extractStats = (option, array) => {
-  const option2 = process.argv[4];  /* Poner la condicion asi estaría bien */
-  if (stats(option) && option2 === '--validate') {
-    let count = array.length;
-    let unique = array.href
-    let stats = {
-      Total: count,
-      Unique: 3,
-      Broken: 1,
+const uniqueLinks = (array) => {
+  let arr = []
+  let arry = []
+  array.forEach(element => {
+    arr.push(element.href);
+  });
+  arr.map((href, i) => {
+    if (arr[href] === arr[i]) {
+      arry.push(href)
     }
-    return stats
-  } else if (option === '--stats') {
-    let count = array.length;
-    let stats = {
-      Total: count,
-      Unique: 3,
-    } 
-    return stats
-  }
+  });
+  return arry.length
 }
 
-/* Llamar función mdLinks que retorna el objeto y da respusta http si se pasa --validate */
-mdLinks.mdLinks(route, options).then((res) => {
-   if (stats(options)) {
-    console.log(extractStats(options, res))
+/* Ejecutar estadísticas de links cuando la opción es --stats y --validate */
+const statsValidate = (array) => {
+  let count = array.length;
+  /* let unique = uniqueLinks(array); */
+  let broken = 0;
+  array.map((e) => {
+    if (e.status >= 400 && e.status <= 599) {
+      broken++
+    }
+  })
+  let stats = {
+    Total: count,
+    Unique: 0,
+    Broken: broken,
   }
-  else {
-    console.log('respuesta array', res);
+  return stats
+}
+
+/* Ejecutar estadísticas de links cuando la opción es --stats */
+const stats = (array) => {
+  let count = array.length;
+  let stats = {
+    Total: count,
+    Unique: 3,
   }
-}).catch(error => console.log('Error en el retorno del objeto', error));
+  return stats
+}
+
+/* Llamar función mdLinks que retorna el objeto y si se pasa --validate y/o --stats */
+mdLinks.mdLinks(route, options).then((res) => res)
+  .then((object) => {
+    if (options === '--validate' && option2 === '--stats') {
+      console.log(statsValidate(object))
+    }
+    else if (options === '--validate') {
+      console.log(object)
+    }
+    else if (options === '--stats') {
+      console.log(stats(object))
+    }
+    else {
+      console.log(object)
+    }
+  })
+
 
